f7d19f20eb14520378afc3fb7d61b496da995181
Mikko Haapoja <me@mikkoh.com>
2014-12-01 00:50:14 -0500

    Should now be fully parsing the test template

--- test/parser.js
+++ test/parser.js
@@ -4,4 +4,8 @@ var data = require( './data/testData' );
 
 var template = fs.readFileSync( __dirname + '/data/testTemplate', 'utf8' );
 
+var starTime = Date.now();
+
 console.log( parser( template, data ) );
+
+console.log( Date.now() - starTime );
